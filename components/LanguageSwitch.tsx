a258042fc6a3f968357d1ad7becf57ed65fd0feb
Dmitrii Gherastovschi <dgherastovschi@gmail.com>
2024-07-26 15:20:33 +0300

    code cleanup

--- components/LanguageSwitch.tsx
+++ components/LanguageSwitch.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { IoLanguageOutline } from "react-icons/io5"
 import { useLocale } from "next-intl"
 import { usePathname, useRouter } from "next/navigation"
 
@@ -24,7 +23,6 @@ export default function LanguageSwitch() {
         className="w-[2.5rem] h-[2.5rem] bg-opacity-80 flex items-center justify-center gap-1 transition-all"
       >
         <span className="sr-only">Change Language</span>
-        {/* <IoLanguageOutline /> */}
 
         <span className="text-sm hover:scale-[1.10] active:scale-105 hover:dark:text-white transition-all">
           {" "}
